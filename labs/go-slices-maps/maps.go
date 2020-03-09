97be3be41b30813acf233fef367b1c007c352e51
Sebastian Cedeno G <bastiangaxiola@gmail.com>
2020-03-08 18:09:26 -0600

    Solved slices and maps tour
    Signed-off-by: Sebastian Cedeno G <bastiangaxiola@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	count := make(map[string]int)
+	for _, word := range strings.Split(s, " ") {
+		count[word]++
+	}
+	return count
+}
+
 func main() {
+	wc.Test(WordCount)
 }
