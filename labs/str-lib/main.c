8ae4041008538f72669f9442ed5a109b724542c7
Sebastian Cedeno G <bastiangaxiola@gmail.com>
2020-03-21 20:32:08 -0600

    Solved string manipulation
    Signed-off-by: Sebastian Cedeno G <bastiangaxiola@gmail.com>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include "strlib.c"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if(argc != 4){
+        perror("Error: format must be ./main <original string> <string to add> <sub-string to find>");
+        return -1;
+    }
+    char* originalStr = argv[1];
+    char* addStr = argv[2];
+    char* sub = argv[3];
+
+    char* newStr = mystradd(originalStr, addStr);
+
+    printf("Initial Lenght      : %d\n", mystrlen(originalStr));
+    printf("New String          : %s\n", newStr);
+    if (mystrfind(newStr, sub)==1){
+        printf("SubString was found : yes\n");
+    } else {
+        printf("SubString was found : no\n");
+    }
+
+    free(newStr);
+    
     return 0;
 }
+
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,57 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int len = 0;
+    for(int i=0;str[i]!='\0';i++){
+        len++;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char* concat;
+    int lenO = mystrlen(origin);
+    int lenA = mystrlen(addition);
+    int len = lenO + lenA;
+    concat = malloc(sizeof(char)*(len+1));
+    lenA = 0;
+    for(int i=0;i<len;i++){
+        if (lenO>0){
+            concat[i]=origin[i];
+            lenO--;
+        } else {
+            concat[i]=addition[lenA];
+            lenA++;
+        }
+    }
+    concat[len] = '\0';
+    return concat;
 }
 
 int mystrfind(char *origin, char *substr){
+    int len = mystrlen(origin);
+    int sub_len = mystrlen(substr);
+    int found = 0;
+    if (len<sub_len || sub_len==0){
+        return 0;
+    }
+    for(int i=0;i<(len+1-sub_len);i++){
+        if (origin[i]==substr[0]){
+            if(sub_len==1){
+                return 1;
+            }
+            found = 1;
+            for(int j=1;j<sub_len;j++){
+                if(origin[i+j]!=substr[j]){
+                    found = 0;
+                    break;
+                }
+            }
+            if (found==1){
+                return 1;
+            }
+        }
+    }
     return 0;
 }
+
