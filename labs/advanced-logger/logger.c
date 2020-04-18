8d07979f7f8da1d0681706f335cc6436e9ecf1cb
Sebastian Cedeno G <bastiangaxiola@gmail.com>
2020-04-17 22:43:28 -0500

    Solved Advanced Logger

--- labs/advanced-logger/logger.c
+++ labs/advanced-logger/logger.c
@@ -1,23 +1,87 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
+
+char logT = 0;
 
 int initLogger(char *logType) {
     printf("Initializing Logger on: %s\n", logType);
-    return 0;
+    if (strcmp(logType, "stdout")==0){
+        logT = 0;
+        return 0;
+    }
+    if (strcmp(logType, "syslog")==0){
+        logT = 1;
+        return 1;
+    }
+    return -1;
 }
 
 int infof(const char *format, ...) {
-    return 0;
+    va_list text;
+    va_start(text, format);
+    if(logT==1){
+        vsyslog(LOG_INFO, format, text);
+        va_end(text);
+        return 1;
+    }
+    textcolor(BRIGHT, GREEN, BLACK);
+    int ok = vfprintf(stdout, format, text);
+    textcolor(RESET, WHITE, BLACK);
+    va_end(text);
+    return ok;
 }
 
 int warnf(const char *format, ...) {
-    return 0;
+    va_list text;
+    va_start(text, format);
+    if(logT==1){
+        vsyslog(LOG_WARNING, format, text);
+        va_end(text);
+        return 1;
+    }
+    textcolor(BRIGHT, YELLOW, BLACK);
+    int ok = vfprintf(stdout, format, text);
+    textcolor(RESET, WHITE, BLACK);
+    va_end(text);
+    return ok;
 }
 
 int errorf(const char *format, ...) {
-    return 0;
+    va_list text;
+    va_start(text, format);
+    if(logT==1){
+        vsyslog(LOG_ERR, format, text);
+        va_end(text);
+        return 1;
+    }
+    textcolor(BRIGHT, MAGENTA, BLACK);
+    int ok = vfprintf(stdout, format, text);
+    textcolor(RESET, WHITE, BLACK);
+    va_end(text);
+    return ok;
 }
 int panicf(const char *format, ...) {
-    return 0;
+    va_list text;
+    va_start(text, format);
+    if(logT==1){
+        vsyslog(LOG_EMERG, format, text);
+        va_end(text);
+        return 1;
+    }
+    textcolor(BRIGHT, RED, WHITE);
+    int ok = vfprintf(stdout, format, text);
+    textcolor(RESET, WHITE, BLACK);
+    va_end(text);
+    return ok;
 }
+
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+}
+
 
--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -4,21 +4,24 @@
 int main(){
 
     // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
+    panicf("Big Chale %s %d\n", "moment", 3);
 
     // stdout logging
     initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
+    panicf("Big Chale %s %d\n", "moment", 3);
 
     // syslog logging
     initLogger("syslog");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
+    panicf("Big Chale %s %d\n", "moment", 3);
 
     return 0;
 }
